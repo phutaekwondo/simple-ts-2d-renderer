811a9f96b80c98a0b35c6f59a1ba979a28d401e1
Lai Nguyen Vinh Phu <lainguyenvinhphu@gmail.com>
2025-11-30 00:32:13 +0700

    implement sprite

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import { Sprite } from "./engine/component/sprite";
 import { Circle } from "./engine/graph/circle";
 import { GameLoop } from "./engine/life-cycle/game-loop";
 import { Node } from "./engine/node";
 import { CanvasRenderer } from "./engine/renderer/canvas-renderer";
-import { MovingSquare } from "./game/moving-square";
 
 // select the canvas
 const canvas = document.getElementById("app") as HTMLCanvasElement;
@@ -25,5 +25,35 @@ function setupScene(root: Node)
     root.addChild(leftCircle);
     leftCircle.addChild(rightCircle);
 
-    root.addChild(new MovingSquare());
+    const sprite = new Sprite();
+    sprite.scaleX = 0.2;
+    sprite.scaleY = 0.2;
+    loadImage("assets/ship.png").then((img) =>
+    {
+        sprite.setImage(img);
+    });
+
+    const sprite2 = new Sprite();
+    sprite2.scaleX = 0.1;
+    sprite2.scaleY = 0.1;
+    sprite2.x;
+
+    loadImage("assets/ship.png").then((img) =>
+    {
+        sprite2.setImage(img);
+    });
+
+    root.addChild(sprite);
+    root.addChild(sprite2);
+}
+
+function loadImage(src: string): Promise<HTMLImageElement>
+{
+    return new Promise((resolve, reject) =>
+    {
+        const img = new Image(); // Create a new HTMLImageElement
+        img.src = src; // Set the source of the image
+        img.onload = () => resolve(img); // Resolve the promise when the image loads
+        img.onerror = (err) => reject(err); // Reject the promise if there's an error
+    });
 }
