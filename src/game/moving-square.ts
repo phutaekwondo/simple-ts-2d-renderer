94af67fd376d00c357ce4ef03ffadec0fcf5bedf
Lai Nguyen Vinh Phu <lainguyenvinhphu@gmail.com>
2025-12-03 12:05:20 +0700

    remove moving square

--- src/game/moving-square.ts
+++
@@ -1,9 +0,0 @@
-import { Square } from "../engine/graph/square";
-
-export class MovingSquare extends Square
-{
-    protected onUpdate(dt: number): void
-    {
-        this.x += 0.1 * dt;
-    }
-}
